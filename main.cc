13f3c960bf0ac49b2b1657a18e0bba78db4eb7b3
Evan Klitzke <evan@eklitzke.org>
2016-10-23 19:47:54 -0700

    fix stuff

--- main.cc
+++ main.cc
@@ -6,10 +6,10 @@
 #include <string>
 #include <vector>
 
-#include <getopt.h>
 #include <systemd/sd-daemon.h>
 
 #include <arpa/inet.h>
+#include <errno.h>
 #include <sys/select.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -19,53 +19,20 @@ const std::vector<int> need_ports{8000, 8001, 8002};
 }
 
 int main(int argc, char **argv) {
-  int ports = 2;
-  const std::string interface = "0.0.0.0";
-  for (;;) {
-    static struct option long_options[] = {{"ports", required_argument, 0, 'p'},
-                                           {0, 0, 0, 0}};
-    int option_index = 0;
-    int c = getopt_long(argc, argv, "p:", long_options, &option_index);
-    if (c == -1) {
-      break;
-    }
-    switch (c) {
-    case 0:
-      if (long_options[option_index].flag != 0) {
-        // if the option set a flag, do nothing
-        break;
-      }
-      break;
-    case 'p':
-      ports = static_cast<int>(std::strtol(optarg, nullptr, 10));
-      break;
-    case '?':
-      // getopt_long should already have printed an error message
-      break;
-    default:
-      abort();
-    }
-  }
-  if (ports < 0) {
-    std::cerr << "negative ports not allowed\n";
-  }
-  if (ports > 2) {
-    std::cerr << "that's too many ports\n";
-  }
-
   // If possible, use the systemd listen socket
+  std::cout << "in main" << std::endl;
   std::vector<int> have_ports;
   const int fd_count = sd_listen_fds(0);
+  std::cout << "fd_count = " << fd_count << std::endl;
   for (int i = 0; i < fd_count; i++) {
     const int fd = SD_LISTEN_FDS_START + i;
     sockaddr_in in_addr;
-    socklen_t socklen;
+    socklen_t socklen = sizeof(in_addr);
     if (getsockname(fd, (sockaddr *)&in_addr, &socklen)) {
-      perror("getsockname()");
+      std::cerr << "getsockname(): " << strerror(errno) << std::endl;
       return 1;
     }
-    const int host_port = static_cast<int>(ntohl(in_addr.sin_port));
-    std::cout << "have port " << host_port << "\n";
+    const int host_port = static_cast<int>(ntohs(in_addr.sin_port));
     have_ports.push_back(host_port);
   }
   for (const auto &need : need_ports) {
@@ -79,10 +46,10 @@ int main(int argc, char **argv) {
     if (have) {
       continue;
     }
-    std::cout << "binding " << need << "\n";
+    std::cout << "binding " << need << std::endl;
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
-      perror("socket()");
+      std::cerr << "socket(): " << strerror(errno) << std::endl;
       return 1;
     }
     sockaddr_in in_addr;
@@ -91,7 +58,7 @@ int main(int argc, char **argv) {
     in_addr.sin_port = htons(need);
     in_addr.sin_addr.s_addr = INADDR_ANY;
     if (bind(sock, (sockaddr *)&in_addr, sizeof(in_addr))) {
-      perror("bind()");
+      std::cerr << "bind(): " << strerror(errno) << std::endl;
       return 1;
     }
   }
